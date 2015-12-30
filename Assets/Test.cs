a7789643f4e2a12d780b291e05fa12ffbb898d47
k1151204 <lastking77@naver.com>
2015-12-30 12:16:26 +0900

    테스트 2단계
    테스트 2단계

---
+++ Assets/Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        Debug.Log("깃허브 테스트");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
